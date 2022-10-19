ad05255c1c366f6332dcd4ff3c6d1422114fb686
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-10-20 06:44:06 +1100

    use of button to load data

--- src/App.js
+++ src/App.js
@@ -2,19 +2,83 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { fetchDataFromApi } from './helper/axiosHelper';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   const [dataFromApi, setDataFromApi] = useState([])
-  useEffect(() => {
+  // useEffect(() => {
+  //   fetchDataFromApi().then(data => {
+  //     setDataFromApi(data)
+  //   })
+  // }, [])
+
+  const handleOnLoad = () => {
     fetchDataFromApi().then(data => {
       setDataFromApi(data)
     })
-  }, [])
+  }
+
+  const handleOnDelete = () => {
+
+  }
+
+  const handleOnAdd = () => {
+
+  }
+
   return (
     <div className="App">
-      {dataFromApi.map((individualData, index) => (
-        individualData.domains
-      ))}
+
+
+      <div>
+        <Button className='btn btn-primary' onClick={handleOnLoad}>LOAD</Button>
+        <Button className='btn btn-danger'>DELETE</Button>
+        <Button className='btn btn-info'>ADD</Button>
+      </div>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Institution Name</th>
+            <th>Website</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {dataFromApi.map((individualData, index) => {
+              return (
+                <div>
+                  <td>{index + 1}</td>
+                  <td>{individualData.name}</td>
+                  <td>{individualData.web_pages}</td>
+                  <td>{individualData.country}</td>
+                </div>
+              )
+            }
+            )}
+
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
