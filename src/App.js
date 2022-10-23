8055646ad61fa68bb193090d0e6e84992868bdd5
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-10-23 17:56:55 +1100

    code refactoring

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import { fetchDataFromApi } from './helper/axiosHelper';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import DisplayTable from './components/DisplayTable';
 
 function App() {
   const [dataFromApi, setDataFromApi] = useState([])
@@ -32,34 +31,7 @@ function App() {
 
   return (
     <div className="App">
-
-      <div className='buttons'>
-        <Button className='btn btn-primary me-1' onClick={handleOnLoad}>LOAD</Button>
-        <Button className='btn btn-danger me-1' onClick={handleOnDelete}>DELETE</Button>
-        <Button className='btn btn-info' onClick={handleOnAdd}>ADD</Button>
-      </div>
-
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Institution Name</th>
-            <th>Website</th>
-            <th>Country</th>
-          </tr>
-        </thead>
-        <tbody>
-
-          {displayData.map((item, i) => (
-            <tr key={item._id}>
-              <td>{i + 1}</td>
-              <td>{item.name}</td>
-              <td><a href={item.web_pages[0]} target='none'>{item.web_pages[0]}</a></td>
-              <td>{item.country}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <DisplayTable handleOnAdd={handleOnAdd} handleOnDelete={handleOnDelete} handleOnLoad={handleOnLoad} displayData={displayData} />
     </div>
   );
 }
--- src/components/DisplayTable.jsx
+++ src/components/DisplayTable.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
 
-const DisplayTable = () => {
-  return <div>DisplayTable</div>;
+const DisplayTable = ({
+  handleOnLoad,
+  handleOnDelete,
+  handleOnAdd,
+  displayData,
+}) => {
+  return (
+    <div>
+      <div className="buttons">
+        <Button className="btn btn-primary me-1" onClick={handleOnLoad}>
+          LOAD
+        </Button>
+        <Button className="btn btn-danger me-1" onClick={handleOnDelete}>
+          DELETE
+        </Button>
+        <Button className="btn btn-info" onClick={handleOnAdd}>
+          ADD
+        </Button>
+      </div>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Institution Name</th>
+            <th>Website</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+        <tbody>
+          {displayData.map((item, i) => (
+            <tr key={item._id}>
+              <td>{i + 1}</td>
+              <td>{item.name}</td>
+              <td>
+                <a href={item.web_pages[0]} target="none">
+                  {item.web_pages[0]}
+                </a>
+              </td>
+              <td>{item.country}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default DisplayTable;
